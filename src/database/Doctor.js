aad155801d05b7ecba46f8f03457e6992db22a46
abhijith1998 <shettyabhijith930@gmail.com>
2021-04-27 14:26:44 +0530

    Doctor.js Model Created

---
+++ src/database/Doctor.js
@@ -0,0 +1,42 @@
+const mongoose=require('mongoose')
+const validator=require('validator')
+
+
+const DoctorSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        toLowerCase:true,
+        validate(email){
+            if(!validator.isEmail(email)){
+                throw new Error('Invalid email address')
+            }
+        }
+    },
+    phone:{
+        required:true,
+        unique:true,
+    },
+    avatar:{
+        type:Buffer,
+        default:[]
+    },
+    qualification:{
+        type:String,
+        required:true,
+    },
+    slots:{
+        default:[],
+        ref:'Slots'
+    }
+})
+
+const Doctor=mongoose.model('Doctor',DoctorSchema)
+
+module.exports=Doctor
