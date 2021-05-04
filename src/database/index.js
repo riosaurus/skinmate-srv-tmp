be281ac47f7d62fecd2d8f64ba2ab81a70690a91
eshwinrio <riopinto487@gmail.com>
2021-05-04 14:08:25 +0530

    test fix

--- src/database/index.js
+++ src/database/index.js
@@ -2,8 +2,8 @@ const Appointment = require('./Appointment');
 const Client = require('./Client');
 const Doctor = require('./Doctor');
 const User = require('./User');
-const Family = require('./Family_member')
+const Family = require('./Family_member');
 
 module.exports = {
-  Appointment, Client, Doctor, User,Family
+  Appointment, Client, Doctor, User, Family,
 };
