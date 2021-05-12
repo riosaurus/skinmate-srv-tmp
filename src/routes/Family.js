3b83346d7a6cfda191c1ac35b88388abdc9cad02
abhijith1998 <shettyabhijith930@gmail.com>
2021-05-12 12:02:18 +0530

    family member docs

--- src/routes/Family.js
+++ src/routes/Family.js
@@ -9,6 +9,114 @@ const router = Router();
  * `http POST` request handler for family member creation.
  * * Requires `access-token `device-id` to be present
  */
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *    Family:
+ *     type: object
+ *     required:
+ *      - user
+ *      - firstName
+ *      - lastName
+ *      - relationship
+ *      - gender
+ *      - dateOfBirth
+ *      - bloodGroup
+ *      - address
+ *     properties:
+ *      _id:
+ *       type: ObjectID
+ *       description: The auto-generated Id for family member
+ *      user:
+ *       type: ObjectID
+ *       description: parent user id
+ *      firstName:
+ *       type: String
+ *       description: first name of family member
+ *      lastName:
+ *       type: String
+ *       description: last name of family member
+ *      relationship:
+ *       type: String
+ *       description: relationship between family member and parent user
+ *      gender:
+ *       type: String
+ *       description: gender of family member
+ *      dateOfBirth:
+ *       type: Date
+ *       description: birth date of family member
+ *      bloodGroup:
+ *       type: String
+ *       description: blood group of family member
+ *      address:
+ *       type: String
+ *       description: address of family member
+ *      insurance:
+ *       type: String
+ *       description: insurance of family member
+ *      emergencyName:
+ *       type: String
+ *       description: emergency name for family member
+ *      emergencyNumber:
+ *       type: String
+ *       description: emergency phone number for family member
+ *     example:
+ *      user: 
+ *      firstName: raj
+ *      lastName: shetty
+ *      relationship: son
+ *      gender: male
+ *      dateOfBirth: 12-08-2016
+ *      bloodGroup: O+ve
+ *      address: billekahalli bangalore
+ *      insurance: REWNIL0057445
+ *      emergencyName: abhi
+ *      emergencyNumber: 7353164950          
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: Family
+ *  description: Family Member Managing API 
+ */
+
+/**
+ * @swagger
+ * /familymember:
+ *  post:
+ *   summary: adding family member
+ *   tags: [Family]
+ *   parameters:
+ *    - name: access-token
+ *      in: header
+ *      required: true
+ *      type: String
+ *    - name: device-id
+ *      in: header
+ *      required: true
+ *      type: String
+ *   requestBody:
+ *    required: true
+ *    content:
+ *     application/json:
+ *      schema:
+ *       $ref: '#/components/schemas/Family'
+ *   responses:
+ *    201:
+ *     description: family member was added successfully
+ *    500:
+ *     description: something server error
+ *    401:
+ *     description: unauthorized access-token
+ *    403:
+ *     description: Operation requires 'device-id'
+ *    406:
+ *     description: validation Error
+ * 
+ *    
+ */
 router.post(
   '/familymember',
   urlencoded({ extended: true }),
@@ -46,7 +154,35 @@ router.post(
  * `http GET` request handler to fetch all family members.
  * * Requires `access-token `device-id` to be present
  */
-
+/**
+ * @swagger
+ * /familymember/all:
+ *  get:
+ *   summary: listing all family member for particular user
+ *   tags: [Family]
+ *   parameters:
+ *    - name: access-token
+ *      in: header
+ *      required: true
+ *      type: String
+ *    - name: device-id
+ *      in: header
+ *      required: true
+ *      type: String
+ *   responses:
+ *    200:
+ *     description: family member fetched successfully
+ *    500:
+ *     description: something server error
+ *    401:
+ *     description: unauthorized access-token
+ *    403:
+ *     description: Operation requires 'device-id'
+ *    404:
+ *     description: family member not found
+ * 
+ *    
+ */
 router.get('/familymember/all',
   middlewares.requireHeaders({ accessToken: true, deviceId: true }),
   middlewares.requireVerification({ phone: true }),
@@ -70,6 +206,39 @@ router.get('/familymember/all',
  * `http DELETE` request handler to delete a family member.
  * * Requires `access-token `device-id` to be present
  */
+
+/**
+ * @swagger
+ * /familymember/{id}:
+ *  delete:
+ *   summary: deleting family member
+ *   tags: [Family]
+ *   parameters:
+ *    - name: access-token
+ *      in: header
+ *      required: true
+ *      type: String
+ *    - name: device-id
+ *      in: header
+ *      required: true
+ *      type: String
+ *    - name: id
+ *      in: path
+ *      required: true
+ *      type: String 
+ *      description: family member id
+ *   responses:
+ *    200:
+ *     description: family meember was deleted successfully
+ *    500:
+ *     description: something server error
+ *    401:
+ *     description: unauthorized access-token
+ *    403:
+ *     description: Operation requires 'device-id'
+ *    404:
+ *     description: family member not found  
+ */
 router.delete('/familymember/:id',
   middlewares.requireHeaders({ accessToken: true, deviceId: true }),
   middlewares.requireVerification({ phone: true }),
@@ -92,6 +261,44 @@ router.delete('/familymember/:id',
 /**
  * `http PATCH` request handler to edit/update a family member.
  * * Requires `access-token `device-id` to be present
+ */
+/**
+ * @swagger
+ * /familymember/{id}:
+ *  patch:
+ *   summary: updating family member details
+ *   tags: [Family]
+ *   parameters:
+ *    - name: access-token
+ *      in: header
+ *      required: true
+ *      type: String
+ *    - name: device-id
+ *      in: header
+ *      required: true
+ *      type: String
+ *    - name: id
+ *      in: path
+ *      required: true
+ *      type: String
+ *      description: doctor id
+ *   requestBody:
+ *    required: true
+ *    content:
+ *     application/json:
+ *      schema:
+ *       $ref: '#/components/schemas/Family'  
+ *   responses:
+ *    200:
+ *     description: Family Member details was updated successfully
+ *    500:
+ *     description: something server error
+ *    401:
+ *     description: unauthorized access-token
+ *    403:
+ *     description: Operation requires 'device-id'
+ *    404:
+ *     description: family member not found    
  */
 router.patch('/familymember/:id',
   urlencoded({ extended: true }),
