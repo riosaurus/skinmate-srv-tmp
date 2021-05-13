1965bed7affaa50fd23c373f88910ef29b3b687d
eshwinrio <riopinto487@gmail.com>
2021-05-13 15:01:41 +0530

    Fixed open handles error for doctor route tests

--- src/routes/__tests__/Doctor.test.js
+++ src/routes/__tests__/Doctor.test.js
@@ -1,13 +1,41 @@
-const request=require('supertest')
-const app=require('../../index')
+/* eslint-disable no-undef */
+const express = require('express');
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const supertest = require('supertest');
+const DoctorsRouter = require('../Doctor');
 
+// May require additional time for downloading MongoDB binaries
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000;
+let mongoServer;
+let server;
 
-//expecting unauthorized request [401] (not having access token and user-agent)
-test('should consider unauthorized ',async()=>{
-  await request(app).post('/doctors').send({
-    name:"pv bhat",
-    email:'pvbhat@gmail.com',
-    phone:"+919999999999",
-    qualification:"MBBS"
-  }).expect(401)
-})
+beforeAll(async () => {
+  mongoServer = new MongoMemoryServer();
+  const mongoUri = await mongoServer.getUri();
+  await mongoose.connect(
+    mongoUri,
+    { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true },
+  );
+  server = express();
+  server.use(DoctorsRouter);
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
+
+describe('Tests for /doctor', () => {
+  it('must reject: unauthorized access', (done) => {
+    supertest(server)
+      .post('/doctors')
+      .send({
+        name: 'pv bhat',
+        email: 'pvbhat@gmail.com',
+        phone: '+919999999999',
+        qualification: 'MBBS',
+      })
+      .expect(401, done);
+  });
+});
