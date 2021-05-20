4845ad0b9d6093c1e2f4a19ae1255c2137f9f160
Rio <75111623+riosaurus@users.noreply.github.com>
2021-05-20 11:02:43 +0530

    Update Appointment.js
    Commented notification triggers to prevent errors.

--- src/database/Appointment.js
+++ src/database/Appointment.js
@@ -42,81 +42,81 @@ const schema = new Schema({
 /**
  * creating notifcation for appoinment creation and reschedule
  */
-schema.pre('save',function(next){
-   this.wasNew=this.isNew
-   next()
-})
-schema.post('save', async function onSave() {
-  try {
-  const time = new Date();
-  let user=this
-  let user_data=await user.populate({
-    path:'userId',
-    select:'fcm_token'
-  }).execPopulate()
-  if (this.wasNew) {
-    try {
-      const { title, body } = NOTIFICATION_ACTION.create.notification;
-    const response = await notification({
-      user: this.userId,
-      type: NOTIFICATION_TYPE.CREATE_APPOINMEMT,
-      fcm_token:user_data.userId.fcm_token,
-      title,
-      body,
-      data: NOTIFICATION_ACTION.create.data.message,
-      date: `${time.toDateString()} ${time.toTimeString()}`,
-    });
-    await response.save();
-    } catch (error) {
-      console.log("unable to create notification"+error)
-    }
-  } else {
-    try {
-      const { title, body } = NOTIFICATION_ACTION.reschedule.notification;
-    const response = await notification({
-      user: this.userId,
-      type: NOTIFICATION_TYPE.UPDATE_APPOINMENT,
-      fcm_token:user_data.userId.fcm_token,
-      title,
-      body,
-      data: NOTIFICATION_ACTION.reschedule.data.message,
-      date: `${time.toDateString()} ${time.toTimeString()}`,
-    });
-    await response.save();
-    } catch (error) {
-      console.log("unable to create notification"+error)
-    }
-  }
-  } catch (error) {
-    console.log(error)
-  }
-});
+// schema.pre('save',function(next){
+//    this.wasNew=this.isNew
+//    next()
+// })
+// schema.post('save', async function onSave() {
+//   try {
+//   const time = new Date();
+//   let user=this
+//   let user_data=await user.populate({
+//     path:'userId',
+//     select:'fcm_token'
+//   }).execPopulate()
+//   if (this.wasNew) {
+//     try {
+//       const { title, body } = NOTIFICATION_ACTION.create.notification;
+//     const response = await notification({
+//       user: this.userId,
+//       type: NOTIFICATION_TYPE.CREATE_APPOINMEMT,
+//       fcm_token:user_data.userId.fcm_token,
+//       title,
+//       body,
+//       data: NOTIFICATION_ACTION.create.data.message,
+//       date: `${time.toDateString()} ${time.toTimeString()}`,
+//     });
+//     await response.save();
+//     } catch (error) {
+//       console.log("unable to create notification"+error)
+//     }
+//   } else {
+//     try {
+//       const { title, body } = NOTIFICATION_ACTION.reschedule.notification;
+//     const response = await notification({
+//       user: this.userId,
+//       type: NOTIFICATION_TYPE.UPDATE_APPOINMENT,
+//       fcm_token:user_data.userId.fcm_token,
+//       title,
+//       body,
+//       data: NOTIFICATION_ACTION.reschedule.data.message,
+//       date: `${time.toDateString()} ${time.toTimeString()}`,
+//     });
+//     await response.save();
+//     } catch (error) {
+//       console.log("unable to create notification"+error)
+//     }
+//   }
+//   } catch (error) {
+//     console.log(error)
+//   }
+// });
 
-/**
- * creating notification for cancel notification
- */
-schema.post('remove', async function onRemove() {
-  try {
-  const time = new Date();
-  let user=this
-  let user_data=await user.populate({
-    path:'userId',
-    select:'fcm_token'
-  }).execPopulate()
-  const { title, body } = NOTIFICATION_ACTION.cancel.notification;
-  const response = await notification({
-    user: this.userId,
-    type: NOTIFICATION_TYPE.CANCEL_APPOINMENT,
-    fcm_token: user_data.userId.fcm_token,
-    title,
-    body,
-    data: NOTIFICATION_ACTION.cancel.data.message,
-    date: `${time.toDateString()} ${time.toTimeString()}`,
-  });
-  await response.save();
-  } catch (error) {
-    console.log(error) 
-  }
-});
+// /**
+//  * creating notification for cancel notification
+//  */
+// schema.post('remove', async function onRemove() {
+//   try {
+//   const time = new Date();
+//   let user=this
+//   let user_data=await user.populate({
+//     path:'userId',
+//     select:'fcm_token'
+//   }).execPopulate()
+//   const { title, body } = NOTIFICATION_ACTION.cancel.notification;
+//   const response = await notification({
+//     user: this.userId,
+//     type: NOTIFICATION_TYPE.CANCEL_APPOINMENT,
+//     fcm_token: user_data.userId.fcm_token,
+//     title,
+//     body,
+//     data: NOTIFICATION_ACTION.cancel.data.message,
+//     date: `${time.toDateString()} ${time.toTimeString()}`,
+//   });
+//   await response.save();
+//   } catch (error) {
+//     console.log(error) 
+//   }
+// });
 
 module.exports = model('Appointment', schema);
